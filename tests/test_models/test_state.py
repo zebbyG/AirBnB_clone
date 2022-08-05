f11585bd4126b68c6db270bde11a2ce465955df1
makaumun <makaumun@gmail.com>
2022-08-05 01:09:57 -0700

    state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -41,12 +41,13 @@ class TestState(unittest.TestCase):
         self.assertTrue(issubclass(type(b), BaseModel))
 
     def test_8_attributes(self):
-        """Tests the attributes of State class."""     
+        """Tests the attributes of State class."""
         attributes = storage.attributes()["State"]
         o = State()
         for k, v in attributes.items():
             self.assertTrue(hasattr(o, k))
             self.assertEqual(type(getattr(o, k, None)), v)
+
 
 if __name__ == "__main__":
     unittest.main()
